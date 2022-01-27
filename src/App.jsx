93606eca3e53c053b847b64ebad2bbcf5549ae87
Jeongs <kimm.jeongs@gmail.com>
2022-01-28 00:47:09 +0900

    :fire:  Prune: App 주석 제거
    Closes #19

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,12 @@ import React from 'react'
 import { ThemeProvider } from '@emotion/react'
 import { GlobalStyle } from '@style/GlobalStyle'
 import theme from '@style/theme'
-import { Table, TableBody } from '@components/base'
 
 const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-
-        {/* <Table thead="상품 기본 정보">
-          <TableBody title="카테고리*">
-            <div>123373498304804938409324903894</div>
-          </TableBody>
-          <TableBody title="필터태그" height="100%">
-            <div>123</div>
-          </TableBody>
-        </Table> */}
       </ThemeProvider>
     </>
   )
