ad646d421ac0bb62b5401fc917223965ec7b8590
ludacirs <ludacirs@naver.com>
2022-01-28 01:55:56 +0900

    :sparkles: Feat: 이미지 업로드 컨테이너 구현
    이미지 업로드시 파일명을 뽑아내서 뿌려주는 기능을 가진 이미지 업로드 컨테이너 구현

--- src/components/base/ImageUploadButton/ImageUploadButton.jsx
+++ src/components/base/ImageUploadButton/ImageUploadButton.jsx
@@ -1,21 +1,23 @@
 import PropTypes from 'prop-types'
 import * as S from './Style'
 
-const ImageUploadButton = ({ isMultiple }) => {
+const ImageUploadButton = ({ isMultiple = true, upload }) => {
+  const handleChange = (e) => {
+    const addFiles = [...e.target.files].map((file) => file.name)
+    upload((files) => [...files, ...addFiles])
+  }
+
   return (
     <S.ImageUploadButtonWrapper>
       + 이미지 첨부
-      <S.FileInput type="file" multiple={isMultiple} />
+      <S.FileInput type="file" multiple={isMultiple} onChange={handleChange} />
     </S.ImageUploadButtonWrapper>
   )
 }
 
 ImageUploadButton.propTypes = {
   isMultiple: PropTypes.bool,
-}
-
-ImageUploadButton.defaultProps = {
-  isMultiple: true,
+  upload: PropTypes.func,
 }
 
 export default ImageUploadButton
