f84947a66a7365c0d09b84533229f6837a381f10
Jeongs <kimm.jeongs@gmail.com>
2022-01-28 14:48:23 +0900

    :bug:  Fix: isImage props 추가, 내부 로직 분기 in ImageUploadButton
    Closes #30

--- src/components/base/ImageUploadButton/ImageUploadButton.jsx
+++ src/components/base/ImageUploadButton/ImageUploadButton.jsx
@@ -1,21 +1,35 @@
 import PropTypes from 'prop-types'
 import * as S from './Style'
 
-const ImageUploadButton = ({ isMultiple = true, upload }) => {
+const ImageUploadButton = ({ isImage = false, isMultiple = true, upload }) => {
   const handleChange = (e) => {
-    const addFiles = [...e.target.files].map((file) => file.name)
-    upload((files) => [...files, ...addFiles])
+    if (isImage) {
+      const imageFileInfo = e.target.files[0]
+      upload({
+        name: imageFileInfo.name,
+        src: URL.createObjectURL(imageFileInfo),
+      })
+    } else {
+      const addFiles = [...e.target.files].map((file) => file.name)
+      upload((files) => [...files, ...addFiles])
+    }
   }
 
   return (
     <S.ImageUploadButtonWrapper>
       + 이미지 첨부
-      <S.FileInput type="file" multiple={isMultiple} onChange={handleChange} />
+      <S.FileInput
+        type="file"
+        accept={isImage ? 'image/*' : '*'}
+        multiple={isMultiple}
+        onChange={handleChange}
+      />
     </S.ImageUploadButtonWrapper>
   )
 }
 
 ImageUploadButton.propTypes = {
+  isImage: PropTypes.bool,
   isMultiple: PropTypes.bool,
   upload: PropTypes.func,
 }
