002ae173f3bb20dcde22e500a17595f3329149e0
ludacirs <ludacirs@naver.com>
2022-01-28 21:29:53 +0900

    ✨ Feat: debounceGenerator 구현

--- src/utils/functions/index.js
+++ src/utils/functions/index.js
@@ -1,2 +1,15 @@
 export const checkTypeOfCss = (property) =>
   typeof property === 'string' ? property : `${property}px`
+
+export const debounceGenerator = (ms) => {
+  let id
+  return (cb) => {
+    if (id) {
+      clearTimeout(id)
+    }
+    id = setTimeout(() => {
+      cb()
+      id = null
+    }, ms)
+  }
+}
