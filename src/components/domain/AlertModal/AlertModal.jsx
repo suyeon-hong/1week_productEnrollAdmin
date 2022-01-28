9a3273ea79c83f0b2903626e9d45c39c54f7c724
Jeongs <kimm.jeongs@gmail.com>
2022-01-28 12:31:30 +0900

    :sparkles:  Feat: AlertModal 구현
    Closes #33

---
+++ src/components/domain/AlertModal/AlertModal.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types'
+import { Modal, Box, Button } from '@components/base'
+import { useModal } from '@hooks'
+import theme from '@style/theme'
+import * as S from './Style'
+
+const AlertModal = ({ isCancelButton, isValidate, children }) => {
+  const { isShowing, toggle, setIsShowing } = useModal(true)
+
+  return (
+    !isValidate &&
+    isShowing && (
+      <Modal isShowing={isShowing} close={toggle}>
+        <Box
+          width={400}
+          height={200}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <S.Text>{children}</S.Text>
+          <S.ButtonWrapper>
+            {' '}
+            {isCancelButton && (
+              <Button
+                color="#fff"
+                background={theme.color.borderDarker}
+                border="inherit"
+                onClick={() => setIsShowing(false)}
+              >
+                취소
+              </Button>
+            )}
+            <Button
+              color="#fff"
+              background={theme.color.purple}
+              border="inherit"
+              onClick={() => setIsShowing(false)}
+            >
+              확인
+            </Button>
+          </S.ButtonWrapper>
+        </Box>
+      </Modal>
+    )
+  )
+}
+
+AlertModal.propTypes = {
+  isCancelButton: PropTypes.bool,
+}
+
+AlertModal.defaultProps = {
+  isCancelButton: true,
+}
+
+export default AlertModal
--- src/components/domain/index.js
+++ src/components/domain/index.js
@@ -1,3 +1,4 @@
 export { default as FilterInput } from './FilterInput/FilterInput'
 export { default as TagSearchModal } from './TagSearchModal/TagSearchModal'
 export { default as TagList } from './TagList/TagList'
+export { default as AlertModal } from './AlertModal/AlertModal'
