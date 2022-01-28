e0b9bd349eecffa8e393fbeb104ca0a916886cb4
kokoball <kbp4154@naver.com>
2022-01-28 16:15:58 +0900

    :bug: (Fix: ImageItemList 중복 제거 해결)
    Closes #28

--- src/components/base/ImageItemList/ImageItemList.jsx
+++ src/components/base/ImageItemList/ImageItemList.jsx
@@ -2,15 +2,23 @@ import PropTypes from 'prop-types'
 import * as S from './Style'
 
 const ImageItemList = ({ itemList, upload }) => {
-  const handleClick = (e) => {
-    upload(itemList.filter((item) => item !== e.target.innerHTML))
+  const images = itemList.map((txt, index) => ({ id: index, title: txt }))
+
+  const handleClick = (id) => {
+    const changeArray = []
+    const list = images.filter((image) => Object.values(image)[0] !== id)
+    list.map((t) => changeArray.push(Object.values(t)[1]))
+    upload(changeArray)
   }
 
   return (
     <S.ImageItemListWrapper>
-      {itemList.map((item) => (
-        <S.ImageItem key={item} onClick={handleClick}>
-          {item}
+      {images.map((image) => (
+        <S.ImageItem
+          key={Object.values(image)[0]}
+          onClick={() => handleClick(Object.values(image)[0])}
+        >
+          {Object.values(image)[1]}
         </S.ImageItem>
       ))}
     </S.ImageItemListWrapper>
