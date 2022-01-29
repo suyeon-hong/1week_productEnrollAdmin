28101524e46cf30b888ec736aee06f02219fe786
Jeongs <kimm.jeongs@gmail.com>
2022-01-29 11:09:19 +0900

    :sparkles:  Feat: Feat: type 추가 in types
    Closes #30

--- src/contexts/OptionContext/reducer.js
+++ src/contexts/OptionContext/reducer.js
@@ -1,11 +1,13 @@
-import { deepCopy } from '@utils/functions'
+import { deepCopy, increaseIndexByOne } from '@utils/functions'
 import {
   ADD_OPTION_SET,
   DELETE_OPTION_SET,
   ADD_OPTION_IMAGE,
+  ADD_OPTION,
   DELETE_OPTION,
-  ADD_OPTION_INFO,
+  UPDATE_OPTION_INFO,
   ADD_ADDITORY_OPTION,
+  DELETE_ADDITORY_OPTION,
 } from './types'
 
 export const initialValue = {
@@ -22,7 +24,12 @@ export const initialValue = {
     },
   ],
   additoryOptions: [],
-  // { index: 0, addOptionName: '', addOptionNormalPrice: 0 }
+}
+
+export const additoryOptionsElement = {
+  index: 0,
+  addOptionName: '',
+  addOptionNormalPrice: 0,
 }
 
 export const optionInfoKey = {
@@ -36,19 +43,21 @@ export const optionInfoKey = {
 export const reducer = (state, { type, payload }) => {
   switch (type) {
     case ADD_OPTION_SET: {
-      return [
-        ...state,
-        { ...payload, index: state.length === 0 ? 0 : state.length },
-      ]
+      return [...state, { ...payload, index: increaseIndexByOne(state) }]
     }
     case DELETE_OPTION_SET: {
       return state.filter((option) => option.index !== +payload)
     }
     case ADD_OPTION_IMAGE: {
       const newState = deepCopy(state)
-      console.log(payload.index)
       newState[payload.index].imageInfo = payload
       return newState
+    }
+    case ADD_OPTION: {
+      const newState = deepCopy(state)
+      console.log(payload)
+
+      return state
     }
     case DELETE_OPTION: {
       const { optionsIndex, deletedOptionInfo } = payload
@@ -56,15 +65,42 @@ export const reducer = (state, { type, payload }) => {
       newState[+optionsIndex].optionInfo = deletedOptionInfo
       return newState
     }
-    case ADD_OPTION_INFO: {
+    case UPDATE_OPTION_INFO: {
       const newState = deepCopy(state)
       const current =
         newState[+payload.optionsIndex].optionInfo[payload.optionInfoIndex]
       Object.entries(payload).forEach(([key, value]) => (current[key] = value))
-      return state
+
+      return newState
     }
-    // case ADD_ADDITORY_OPTION: {
-    // }
+    case ADD_ADDITORY_OPTION: {
+      const { optionsIndex, optionInfoIndex } = payload
+      const newState = deepCopy(state)
+      const additoryOptionsArray = newState[+optionsIndex].additoryOptions
+
+      if (!additoryOptionsArray[optionInfoIndex]) {
+        additoryOptionsArray[optionInfoIndex] = []
+      }
+      additoryOptionsArray[optionInfoIndex].push({
+        ...additoryOptionsElement,
+        index: increaseIndexByOne(additoryOptionsArray[optionInfoIndex]),
+      })
+      return newState
+    }
+    case DELETE_ADDITORY_OPTION: {
+      const { optionsIndex, optionInfoIndex, clickedIndex } = payload
+      const newState = deepCopy(state)
+      const additoryOptionsArrayElement =
+        newState[+optionsIndex].additoryOptions[optionInfoIndex]
+      const deleteAdditoryOptionsArrayElement =
+        additoryOptionsArrayElement.filter(
+          (element) => element.index !== clickedIndex,
+        )
+      newState[+payload.optionsIndex].additoryOptions[optionInfoIndex] =
+        deleteAdditoryOptionsArrayElement
+
+      return newState
+    }
     default: {
       throw new Error('type is inValid')
     }
