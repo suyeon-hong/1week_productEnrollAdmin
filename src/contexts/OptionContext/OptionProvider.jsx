c18142adcf55bc59d749639aeefbc6d9f2ccff41
Jeongs <kimm.jeongs@gmail.com>
2022-01-30 20:13:49 +0900

    :recycle:  Refactor: OptionPrivder js -> jsx로 변경

---
+++ src/contexts/OptionContext/OptionProvider.jsx
@@ -0,0 +1,13 @@
+import { createContext, useReducer } from 'react'
+import { initialValue, reducer } from './reducer'
+
+const OptionContext = createContext(initialValue)
+const { Provider } = OptionContext
+
+const OptionProvider = ({ children }) => {
+  const [options, dispatch] = useReducer(reducer, [])
+
+  return <Provider value={{ options, dispatch }}>{children}</Provider>
+}
+
+export { OptionContext, OptionProvider }
